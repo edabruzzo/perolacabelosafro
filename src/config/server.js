b8142617c2d49e131c56eba9e4cb4d205718e49f
Emmanuel D'Abruzzo <emmanuel.oliveira3@gmail.com>
2020-01-18 20:43:50 -0300

    Separa responsabilidade de roteamento no servidor ao arquivo rotas.js

--- src/config/server.js
+++ src/config/server.js
@@ -3,17 +3,14 @@ const express = require('express')
 //const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const rotas = require('../app/rotas/rotas.js');
 
 module.exports = function(){
 
 
 	var app = express();
+	rotas(app);
   		
-	//app.use(express.static(path.join(__dirname, 'public')))
-  	//app.set('views', path.join(__dirname, 'views'))
-  		//.set('view engine', 'ejs')
-  	//app.get('/', (req, res) => res.render('pages/index'))
-  	//app.get('/cool', (req, res) => res.send(cool()))
   	app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
