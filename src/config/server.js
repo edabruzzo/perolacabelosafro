e112958e61d7a5e611bdaf536edf2bc431be9fb4
Emmanuel D'Abruzzo <emmanuel.oliveira3@gmail.com>
2020-01-22 12:02:20 -0300

    Adiciona tratamento de erro ao app

--- src/config/server.js
+++ src/config/server.js
@@ -26,6 +26,29 @@ module.exports = function(){
 	app.use(express.static(path.join(__dirname, 'public')))
 
 
+// Error Handler ===============================================================
+// middleware that logs the error
+app.use((err, req, res, next) => {
+	console.error(err)
+	next(err)
+  })
+  
+  // middleware that responds to the 500 error
+  // The 500 error is associated with a requesting a file that does not exist
+  app.use((err, req, res, next) => {
+	res.status(500).send("Internal server error.")
+  })
+  
+  // middleware that responds to the 404 error
+  // The 404 error is associated with a GET request failure
+  app.use((req, resp) => {
+	resp.status(404).send("Page not found!")
+  })
+
+
+
+
+
 	app.listen(app.settings.port, () => console.log(`Listening on ${ PORT }`))
 
 
